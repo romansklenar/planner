e198ea7217f5f8df7ff5b6f169ad4f80fccc0138
Roman Sklenar <mail@romansklenar.cz>
2010-03-31 03:26:36 +0200

    routes small refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ ActionController::Routing::Routes.draw do |map|
   map.signup 'signup', :controller => 'users', :action => 'new'
   map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
   map.login 'login', :controller => 'user_sessions', :action => 'new'
+  map.forgot_password  'forgot-password', :controller => 'password_resets', :action => 'new'
+
+  map.resource :account, :controller => "users"
+  map.resources :password_resets
+  map.resources :users
   map.resources :user_sessions
 
-  map.resources :users
-
-  map.resources :projects, :tasks
+  map.resources :projects
+  map.resources :tasks
   map.root :projects
 end
