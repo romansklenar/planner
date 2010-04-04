3dddcd891279b633afa6c17f475999c9682cfafe
Roman Sklenar <mail@romansklenar.cz>
2010-04-04 15:42:25 +0200

    activated default admin user to allow log in

--- db/migrate/20100401231710_add_active_to_users.rb
+++ db/migrate/20100401231710_add_active_to_users.rb
@@ -1,6 +1,10 @@
 class AddActiveToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :active, :boolean, :default => false, :null => false
+
+    admin = User.find_by_login('admin')
+    admin.activate!
+    admin.save
   end
 
   def self.down
