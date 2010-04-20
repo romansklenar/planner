f12b8340767a0f32ae489da851dd0c5e262e8f0a
Roman Sklenar <mail@romansklenar.cz>
2010-04-20 14:08:02 +0200

    added AJAX indicator

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+Ajax.Responders.register({
+  onCreate: function(){
+    $('indicator').show();
+  },
+  onComplete: function() {
+    if(Ajax.activeRequestCount == 0)
+      $('indicator').hide();
+  }
+});
